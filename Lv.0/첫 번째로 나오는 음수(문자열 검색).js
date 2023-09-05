4e8a4286462760df5b65ce791a204f7058c4d641
ujkey <135115849+ujkey@users.noreply.github.com>
2023-09-05 21:11:00 +0900

    Update: 문자열 검색 > lastIndexOf 내용 추가

--- Lv.0/첫 번째로 나오는 음수(문자열 검색).js
+++ Lv.0/첫 번째로 나오는 음수(문자열 검색).js
@@ -4,11 +4,14 @@
     1. Array.prototype.find()
         - 주어진 판별 함수를 만족하는 첫 번째 요소의 '값'을 반환 / undefined
 
-    2. String.prototype.indexOf(searchValue)
+    2. Array.prototype.findIndex()
+        - 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 '인덱스'를 반환 / -1
+
+    3. String.prototype.indexOf(searchValue)
         - String 객체에서 주어진 값과 일치하는 첫 번째 '인덱스'를 반환 / -1
 
-    3. Array.prototype.findIndex()
-        - 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 '인덱스'를 반환 / -1
+    4. String.prototype.lastIndexOf(searchValue[, fromIndex])
+        - 주어진 값과 일치하는 부분을 fromIndex로부터 역순으로 탐색하여, 최초로 마주치는 인덱스를 반환 / -1
 
     reference
         - https://velog.io/@zwonlala/%EB%B0%B0%EC%97%B4-%EB%82%B4%EC%9E%A5%ED%95%A8%EC%88%98-indexOf-findIndex-find

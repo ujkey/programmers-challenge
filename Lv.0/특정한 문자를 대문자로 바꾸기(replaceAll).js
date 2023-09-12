70f36b91ea6ca97866e1f598e9a2a778e62f2514
ujkey <135115849+ujkey@users.noreply.github.com>
2023-09-12 11:56:40 +0900

    Update: replace > replacement 내용 추가

--- Lv.0/특정한 문자를 대문자로 바꾸기(replaceAll).js
+++ Lv.0/특정한 문자를 대문자로 바꾸기(replaceAll).js
@@ -2,6 +2,7 @@
     1. String.prototype.replaceAll(pattern, replacement)
         - pattern의 모든 일치 항목이 replacement로 대체된 새 문자열을 반환
         - pattern은 문자열 또는 RegExp일 수 있음
+        - replacement는 콜백일 수 있음
 */
 
 function solution(my_string, alp) {

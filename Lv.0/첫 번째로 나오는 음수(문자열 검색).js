2a616fb5a48e396cf5f8e4b14565bd63836c1938
ujkey <135115849+ujkey@users.noreply.github.com>
2023-10-05 14:51:35 +0900

    Update: indexOf() 매개변수 내용 추가

--- Lv.0/첫 번째로 나오는 음수(문자열 검색).js
+++ Lv.0/첫 번째로 나오는 음수(문자열 검색).js
@@ -7,9 +7,11 @@
     2. Array.prototype.findIndex()
         - 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 '인덱스'를 반환 / -1
 
-    3. String.prototype.indexOf(searchValue)
+    3. String.prototype.indexOf(searchValue[, formIndex])
         - String 객체에서 주어진 값과 일치하는 첫 번째 '인덱스'를 반환 / -1
         - 순방향 탐색
+        - searchValue: 찾으려는 문자열
+        - fromIndex: 문자열에서 찾기 시작하는 위치를 나타내는 인덱스 값
 
     4. String.prototype.lastIndexOf(searchValue[, fromIndex])
         - 주어진 값과 일치하는 부분을 fromIndex로부터 역순으로 탐색하여, 최초로 마주치는 인덱스를 반환 / -1

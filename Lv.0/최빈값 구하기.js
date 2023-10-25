4a6613550887cff2e8b92e04595cc9c893698d3d
ujkey <135115849+ujkey@users.noreply.github.com>
2023-10-25 12:58:34 +0900

    day63

--- Lv.0/최빈값 구하기.js
+++ Lv.0/최빈값 구하기.js
@@ -1,5 +1,7 @@
-// unresolved
-// 37.5
+/*
+unresolved
+37.5
+
 function solution(array) {
     var answer = 0; //최빈값 : 가장 자주 나오는 값
 
@@ -14,4 +16,29 @@ function solution(array) {
     }, 0);
 
     return answer;
+}
+*/
+
+function frequencisesBy(array) {
+    const result = {};
+    array.forEach((v) => {
+        const key = v;
+        result[key] ? (result[key] += 1) : (result[key] = 1);
+    });
+    return result;
+}
+
+function solutionA(array) {
+    const obj = frequencisesBy(array);
+    const values = Object.values(frequencisesBy(array));
+    const mVal = Math.max(...values);
+    return (values.filter((v)=> v===mVal).length > 1) ? -1 : Object.keys(obj)[values.indexOf(mVal)]*1;
+}
+
+// refactoring
+function solutionB(array) {
+    let m = new Map();
+    for(let v of array) m.set(v, (m.get(v) || 0)+1);
+    m = [...m].sort((a,b) => b[1]-a[1]); //DESC
+    return m.length===1 || m[0][1] !== m[1][1] ? m[0][0] : -1;
 }

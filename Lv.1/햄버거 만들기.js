c4bf123c6e8d2c0e4edc864cc8525b6683494412
ujkey <135115849+ujkey@users.noreply.github.com>
2023-12-12 10:49:11 +0900

    day102

--- Lv.1/햄버거 만들기.js
+++ Lv.1/햄버거 만들기.js
@@ -1,9 +1,26 @@
+// 시간 복잡도 개선 - stack 활용
+function solutionB(ingredient) {
+    let answer = 0;
+    const stack = [];
+
+    for(let i of ingredient) {
+        stack.push(i);
+
+        if(stack.slice(-4).join('') === '1231') {
+            stack.splice(-4);
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
 /*
     채점 결과 - 실패(시간 초과)
     정확성: 72.2
     합계: 72.2 / 100.0
 */
-function solution(ingredient) {
+function solutionA(ingredient) {
     let answer = 0;
     let numStr = ingredient.join('');
 

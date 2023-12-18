8317dcd37c447c87350d87a6215e2511c4b9f493
ujkey <135115849+ujkey@users.noreply.github.com>
2023-12-18 11:24:41 +0900

    day104

--- Lv.1/덧칠하기.js
+++ Lv.1/덧칠하기.js
@@ -1,4 +1,27 @@
 /*
+    [Array.prototype.fill(value, start, end)]
+        배열의 인덱스 범위 내에 있는 모든 요소를 정적 값으로 변경한다
+        수정된 배열을 반환
+*/
+function solutionB(n, m, section) {
+    let count = 0;
+    const arr = Array(n-1).fill(null);
+
+    section.forEach((s) => {
+        arr[s] = 1; //덧칠 해야할 구역 표시
+    })
+
+    section.forEach((s) => {
+        if(arr[s]) {
+            arr.fill(null, s, s+m);
+            count++;
+        }
+    })
+
+    return count;
+}
+
+/*
     채점 결과
     정확성: 100.0
     합계: 100.0 / 100.0

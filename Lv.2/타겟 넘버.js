1dd41abf243ffbaa1ead9a79e4e493f45cce07f9
ujkey <ryujk6894@gmail.com>
2024-03-18 08:02:15 +0900

    Refactor: 그래프 탐색(DFS,BFS) > 타겟 넘버(큐 자료구조)

--- Lv.2/타겟 넘버.js
+++ Lv.2/타겟 넘버.js
@@ -1,6 +1,8 @@
 // 깊이, 너비 우선 탐색(DFS/BFS) >
 // https://school.programmers.co.kr/learn/courses/30/lessons/43165
-function solution(numbers, target) {
+
+// 1. 재귀호출 활용
+function solutionA(numbers, target) {
     // numbers : number[]
     let answer = 0; // 정수를 조합(+,-)하여 target 넘버를 만들 수 있는 방법의 수
     const n = numbers.length; // 최대 깊이
@@ -20,4 +22,32 @@ function solution(numbers, target) {
 
     return answer;
 }
+
+// 2. 큐 자료구조 활용
+function solutionB(numbers, target) {
+    let answer = 0; // 타겟 넘버를 완성한 횟수
+    const n = numbers.length; // 최대 깊이
+
+    const queue = [];
+    queue.push([0, 0]); // [L, sum]
+
+    while(queue.length) {
+        const [curL, curSum] = queue.shift();
+
+        // 종료 조건
+        if(curL === n) {
+            if(curSum === target) {
+                answer++;
+            }
+            continue; // return X
+        }
+
+        queue.push([curL+1, curSum+numbers[curL]]);
+        queue.push([curL+1, curSum-numbers[curL]]);
+    }
+
+    return answer;
+}
+
+
 
